35478fcfe9cfd79f17307d2e439b555334f310e7
hanzalahsamana <hanzalahsamana789@gmail.com>
2024-10-17 14:33:13 +0500

    solve image url issue

--- Config/uploadConfig.js
+++ Config/uploadConfig.js
@@ -1,16 +1,16 @@
-const multer = require('multer');
-const { cloudinary } = require('./cloudinaryConfig');  // Import cloudinaryConfig
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require("multer");
+const { cloudinary } = require("./cloudinaryConfig"); // Import cloudinaryConfig
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 // Configure Cloudinary storage for Multer
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'products',  // Folder in Cloudinary where images will be stored
-    allowedFormats: ['jpg', 'jpeg', 'png'],  // Allowed image formats
+    folder: "products", // Folder in Cloudinary where images will be stored
+    allowedFormats: ["jpg", "jpeg", "png"], // Allowed image formats
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage }).array("image", 2); // Adjust the number according to how many files you want to upload
 
-module.exports = { upload };  // Export the configured 'upload'
+module.exports = { upload }; // Export the configured 'upload'
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
+const { upload } = require("../Config/uploadConfig");
 const { postProductData, getProductData } = require("../Utils/product");
 const express = require("express");
 
 
 const routes = express.Router();
 
-routes.post("/addProduct", postProductData);
+routes.post("/addProduct",upload, postProductData);
 routes.get("/getProducts", getProductData);
 
 
