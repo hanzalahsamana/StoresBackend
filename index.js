decb1792431410ac01f4d6c1a8215818bd2c36cb
hanzalahsamana <hanzalahsamana789@gmail.com>
2025-02-20 19:50:28 +0500

    nginx setup

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use("/api/v1", withoutParams);
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 1234;
-  app.listen(8081, () => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
   });
 }
