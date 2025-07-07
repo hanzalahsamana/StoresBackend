67931c68094cda984c0a0fc87488375a349b1ea7
hanzalahsamana <hanzalahsamana789@gmail.com>
2025-07-07 14:47:38 +0500

    code optimization of gethomepage

--- Controller/pages/homePage.js
+++ Controller/pages/homePage.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const { CollectionModel } = require("../../Models/CollectionModel");
-const { ProductModel } = require("../../Models/ProductModel");
 const { SectionModel } = require("../../Models/SectionsModel");
+const { ProductModel } = require("../../Models/ProductModel");
+const { CollectionModel } = require("../../Models/CollectionModel");
 
 module.exports = {
     getHomePageData: async (req, res) => {
@@ -10,77 +10,64 @@ module.exports = {
 
             const sections = await SectionModel.find({ storeRef: storeId }).sort({ order: 1 }).lean();
 
-            const collectionIds = new Set();
-            const productIds = new Set();
-            const categoryIds = new Set();
+            const collectionIds = [];
+            const productIds = [];
+            const categoryIds = [];
 
-            sections.forEach(sec => {
-                const content = sec.content;
-
+            for (const sec of sections) {
                 if (sec.type === "feature_collection") {
-                    (content?.collections || []).forEach(id => collectionIds.add(id.toString()));
+                    collectionIds.push(...(sec.content?.collections || []));
                 }
-
                 if (sec.type === "feature_product") {
-                    if (content?.selectedProducts?.length)
-                        content.selectedProducts.forEach(id => productIds.add(id.toString()));
-                    else if (content?.selectedcollections?.length)
-                        content.selectedcollections.forEach(id => categoryIds.add(id.toString()));
+                    if (sec.content?.selectedProducts?.length) {
+                        productIds.push(...sec.content.selectedProducts);
+                    } else if (sec.content?.selectedcollections?.length) {
+                        categoryIds.push(...sec.content.selectedcollections);
+                    }
                 }
-            });
+            }
 
             const [collections, selectedProducts, categoryProducts] = await Promise.all([
-                CollectionModel.find({ _id: { $in: [...collectionIds] } }).lean(),
-                productIds.size ? ProductModel.find({ _id: { $in: [...productIds] } }).lean() : [],
-                categoryIds.size
-                    ? ProductModel.find({
-                        collections: { $in: [...categoryIds].map(id => new mongoose.Types.ObjectId(id)) },
-                    }).lean()
-                    : [],
+                CollectionModel.find({ _id: { $in: collectionIds } }).lean(),
+                ProductModel.find({ _id: { $in: productIds } }).lean(),
+                ProductModel.find({ collections: { $in: categoryIds } }).lean(),
             ]);
 
             const collectionsMap = Object.fromEntries(collections.map(c => [c._id.toString(), c]));
             const productMap = Object.fromEntries(selectedProducts.map(p => [p._id.toString(), p]));
+            const categoryProductMap = {};
 
-            const categoryProductMap = categoryProducts.reduce((acc, product) => {
-                product.collections?.forEach(cid => {
-                    cid = cid.toString();
-                    if (categoryIds.has(cid)) {
-                        acc[cid] = acc[cid] || [];
-                        acc[cid].push(product);
-                    }
-                });
-                return acc;
-            }, {});
+            for (const product of categoryProducts) {
+                for (const cid of product.collections || []) {
+                    const key = cid.toString();
+                    if (!categoryProductMap[key]) categoryProductMap[key] = [];
+                    categoryProductMap[key].push(product);
+                }
+            }
 
-            sections.forEach(sec => {
-                const content = sec.content;
-
+            for (const sec of sections) {
                 if (sec.type === "feature_collection") {
-                    content.collections = (content.collections || [])
+                    sec.content.collections = (sec.content.collections || [])
                         .map(id => collectionsMap[id.toString()])
                         .filter(Boolean);
                 }
-
                 if (sec.type === "feature_product") {
-                    if (content?.selectedProducts?.length) {
-                        content.selectedProducts = content.selectedProducts
+                    if (sec.content?.selectedProducts?.length) {
+                        sec.content.selectedProducts = sec.content.selectedProducts
                             .map(id => productMap[id.toString()])
                             .filter(Boolean);
                     }
-
-                    if (content?.selectedcollections?.length) {
-                        content.selectedcollections = content.selectedcollections
+                    if (sec.content?.selectedcollections?.length) {
+                        sec.content.selectedcollections = sec.content.selectedcollections
                             .flatMap(cid => categoryProductMap[cid.toString()] || []);
                     }
                 }
-            });
+            }
 
-            return res.status(200).json({ success: true, data: sections });
-
+            res.status(200).json({ success: true, data: sections });
         } catch (error) {
-            console.error("Error fetching home page data:", error?.message || error);
-            return res.status(500).json({ message: "Something went wrong!", success: false });
+            console.error("Error fetching home page data:", error);
+            res.status(500).json({ success: false, message: "Something went wrong!" });
         }
-    },
+    }
 };
